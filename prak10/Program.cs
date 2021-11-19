383e742d43b2e2bc7a99d4e30e12175c5894310b
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2021-11-19 09:21:01 +0300

    Add prak10

---
+++ prak10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 2;
+            int n = 10;
+
+            Console.WriteLine(Math.Round(Gauss(a, b, n),3));
+        }
+
+        public static double Gauss(double a, double b, int n)
+        {
+            double result = 0;
+            double h = (b - a) / n;
+            for (double i = a; i < b; i += h)
+            {
+                result += Calc(i + h / 2 - h / ( 2 * Math.Sqrt(3))) + Calc(i + h / 2 + h / (2 * Math.Sqrt(3)));
+            }
+
+            return result * h / 2;
+        }
+
+        public static double Calc(double x)
+        {
+            return Math.Sqrt(1 + x * x);
+        }
+    }
+}
