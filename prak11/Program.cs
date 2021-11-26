490f2284a5b33c9ccbedf4a0f22fab03b3048faf
bozinandrey1@yandex.ru <bozinandrey1@yandex.ru>
2021-11-26 12:47:10 +0300

    Add prak 11

---
+++ prak11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace prak11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 1;
+            double h = 0.1;
+            double b = 1;
+
+            Console.WriteLine(Eyler(x, y, h, b));
+        }
+
+        static string Eyler(double x, double y, double h, double b)
+        {
+            string result = "";
+
+            while (x <= b - h)
+            {
+                y += h * Calc(x, y);
+                x += h;
+
+                result += $"{y}{Environment.NewLine}";
+            }
+
+            return result;
+        }
+
+        static double Calc(double x, double y)
+        {
+            return (x + x * y * y)/(y - x * x * y);
+        }
+    }
+}
